a7f8bc638a3af7acfaa3f216110e1ecbcc4e899c
Sandersd <sandersd27@gmail.com>
2018-01-21 18:05:01 -0800

    add container to previews

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@
 function setup() {
     createSketch('main-canvas', 1);
     createSketch('canvas-1997', 2);
+    createSketch('canvas-kelvin', 2);
+    createSketch('canvas-aden', 2);
+    createSketch('canvas-brannan', 2);
+    createSketch('canvas-nashville', 2);
 }
 
 function draw() {
